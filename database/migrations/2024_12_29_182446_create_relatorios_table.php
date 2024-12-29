e62977c02b38b58af4b56d224fedd2499c61fbba
anapfreitas <anapsantosfreitas@hotmail.com>
2024-12-29 15:38:37 -0300

    Criação dos migrations

---
+++ database/migrations/2024_12_29_182446_create_relatorios_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('relatorios', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('funcionario_id')->constrained()->onDelete('cascade'); 
+            $table->date('periodo_inicio');
+            $table->date('periodo_fim');
+            $table->integer('total_horas_trabalhadas');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('relatorios');
+    }
+};
